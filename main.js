c17f9fb6b87254496242b4075e91923d42edd5a9
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-11 17:28:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,12 +14,6 @@ fetch('data/places.geojson')
         }
       }
     }).addTo(map);
-  });
-
-const map = L.map('map', {
-  crs: L.CRS.Simple,
-  minZoom: -2
-});
 
 const imageWidth = 4096;
 const imageHeight = 3072;
@@ -28,8 +22,4 @@ const imageBounds = [[0, 0], [imageHeight, imageWidth]];
 L.imageOverlay('adenai_map_01.jpg', imageBounds).addTo(map);
 map.fitBounds(imageBounds);
 });
-
-var bounds = [[0,0], [4096,3072]];
-L.imageOverlay('adenai_map_01', bounds).addTo(map);
-map.fitBounds(bounds);
 
