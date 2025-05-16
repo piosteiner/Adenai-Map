a37b70ce709b6df81624ca1bc1cce40bddef9328
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-16 16:45:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -52,8 +52,8 @@ const shipBounds = [
   [1032 + 30, 1916 + 30]   // Northeast corner
 ];
 
-//Ship Icon 1/2
-L.imageOverlay('images/vsuzh_ship_draft.png', shipBounds, {
+//Ship Icon 2/2
+L.imageOverlay('images/vsuzh_ship_draft_mirrored.png', shipBounds, {
   interactive: false
 }).addTo(map);
 
