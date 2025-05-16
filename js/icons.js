dba789c5ade36e69c342445a4f247564fd563f15
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-17 01:32:36 +0200

    map extension

--- js/icons.js
+++ js/icons.js
@@ -1,3 +1,14 @@
+//Map Expansion
+const mapextension1 = [
+  [1050 - 1024, 3072 - 1024],  // Southwest corner
+  [1050 + 1024, 3072 + 1024]   // Northeast corner
+];
+
+//2048p Expansion
+L.imageOverlay('images/mapextension.PNG', mapextension1, {
+  interactive: false
+}).addTo(map);
+
 //Ship Icon
 const ship1 = [
   [1032 - 30, 1916 - 30],  // Southwest corner
