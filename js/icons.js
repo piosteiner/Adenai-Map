0e6e287a77159baeff4a27d965359cd8dd49dad9
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-17 01:09:53 +0200

    images update

--- js/icons.js
+++ js/icons.js
@@ -1,28 +1,51 @@
 //Ship Icon
-const shipBounds = [
+const ship1 = [
   [1032 - 30, 1916 - 30],  // Southwest corner
   [1032 + 30, 1916 + 30]   // Northeast corner
 ];
 
 //Ship Icon
-L.imageOverlay('images/vsuzh_ship_draft_mirrored.png', shipBounds, {
+L.imageOverlay('images/vsuzh_ship_mirrored.png', ship1, {
   interactive: false
 }).addTo(map);
 
-
-//Atlantis Icon 1 and 2
-const atlantisGeneral1 = [
+//Ship Icon
+const atlantisBubble = [
   [975 - 40, 2210 - 40],  // Southwest corner
   [975 + 40, 2210 + 40]   // Northeast corner
 ];
 
-const atlantisGeneral2 = [
+//Ship Icon
+L.imageOverlay('images/atlantis_bubble.png', atlantisBubble, {
+  interactive: false
+}).addTo(map);
+
+//Ship Icon
+const atlantisClouds = [
   [1200 - 40, 2210 - 40],  // Southwest corner
   [1200 + 40, 2210 + 40]   // Northeast corner
 ];
 
-const atlantisImage = 'images/atlantis_general.png';
-const atlantisOptions = { interactive: false };
+//Ship Icon
+L.imageOverlay('images/atlantis_clouds.png', atlantisClouds, {
+  interactive: false
+}).addTo(map);
 
-L.imageOverlay(atlantisImage, atlantisGeneral1, atlantisOptions).addTo(map);
-L.imageOverlay(atlantisImage, atlantisGeneral2, atlantisOptions).addTo(map);
+
+
+//Atlantis Icon 1 and 2
+//const atlantisGeneral1 = [
+//  [975 - 40, 2210 - 40],  // Southwest corner
+//  [975 + 40, 2210 + 40]   // Northeast corner
+//];
+
+//const atlantisGeneral2 = [
+//  [1200 - 40, 2210 - 40],  // Southwest corner
+//  [1200 + 40, 2210 + 40]   // Northeast corner
+//];
+
+//const atlantisImage = 'images/atlantis_general.png';
+//const atlantisOptions = { interactive: false };
+
+//L.imageOverlay(atlantisImage, atlantisGeneral1, atlantisOptions).addTo(map);
+//L.imageOverlay(atlantisImage, atlantisGeneral2, atlantisOptions).addTo(map);
