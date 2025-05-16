def396b0b03683addf530bf120c033797fc14d10
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-16 16:55:56 +0200

    Update icons.js

--- js/icons.js
+++ js/icons.js
@@ -8,3 +8,15 @@ const shipBounds = [
 L.imageOverlay('images/vsuzh_ship_draft_mirrored.png', shipBounds, {
   interactive: false
 }).addTo(map);
+
+
+//Atlantis Icon 1/2
+const atlantisGeneral = [
+  [975 - 40, 2210 - 40],  // Southwest corner
+  [975 + 40, 2210 + 40]   // Northeast corner
+];
+
+//Atlantis Icon 2/2
+L.imageOverlay('images/atlantis_general.png', atlantisGeneral, {
+  interactive: false
+}).addTo(map);
