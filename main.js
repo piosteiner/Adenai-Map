09d1cce673a28ebc3d3dd6f3a40491655b6c0384
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-16 14:26:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const DotOrange = L.icon({
 //Define Ship Image
 const ShipIcon = L.icon({
   iconUrl: 'images/vsuzh_ship_draft.png', // Replace with your image path
-  iconSize: [30, 30],            // Automatically scale width, fixed height
+  iconSize: null,            // "["30, 30]"" wouldAutomatically scale width, fixed height
   iconAnchor: [15, 15],            // Adjust to center it properly
   popupAnchor: [0, -15]
 });
