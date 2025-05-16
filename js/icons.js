c7eaca57328a923c4c15f84c80b22009fdd6e8a1
piosteiner <34060421+piosteiner@users.noreply.github.com>
2025-05-16 17:06:17 +0200

    Update icons.js

--- js/icons.js
+++ js/icons.js
@@ -1,22 +1,28 @@
-//Ship Icon 1/2
+//Ship Icon
 const shipBounds = [
   [1032 - 30, 1916 - 30],  // Southwest corner
   [1032 + 30, 1916 + 30]   // Northeast corner
 ];
 
-//Ship Icon 2/2
+//Ship Icon
 L.imageOverlay('images/vsuzh_ship_draft_mirrored.png', shipBounds, {
   interactive: false
 }).addTo(map);
 
 
-//Atlantis Icon 1/2
-const atlantisGeneral = [
+//Atlantis Icon 1 and 2
+const atlantisGeneral1 = [
   [975 - 40, 2210 - 40],  // Southwest corner
   [975 + 40, 2210 + 40]   // Northeast corner
 ];
 
-//Atlantis Icon 2/2
-L.imageOverlay('images/atlantis_general.png', atlantisGeneral, {
-  interactive: false
-}).addTo(map);
+const atlantisGeneral2 = [
+  [1200 - 40, 2210 - 40],  // Southwest corner
+  [1200 + 40, 2210 + 40]   // Northeast corner
+];
+
+const atlantisImage = 'images/atlantis_general.png';
+const atlantisOptions = { interactive: false };
+
+L.imageOverlay(atlantisImage, atlantisGeneral1, atlantisOptions).addTo(map);
+L.imageOverlay(atlantisImage, atlantisGeneral2, atlantisOptions).addTo(map);
