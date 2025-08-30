60562fbcb78e7debaccbb082ed534014d858e907
piosteiner <pioginosteiner@gmail.com>
2025-08-30 19:19:14 +0200

    Update location-system.js

--- src/systems/location-system.js
+++ src/systems/location-system.js
@@ -582,7 +582,7 @@ class LocationSystem {
                     if (region) {
                         const regionInfo = this.locationRegions[region] || this.locationRegions.unknown;
                         if (type) popupContent += ' • '; // Add separator if both type and region
-                        popupContent += `<span class="popup-region">Region: ${regionInfo.label}</span>`;
+                        popupContent += `<span class="popup-region">Region fehlt ${regionInfo.label}</span>`;
                     }
                     
                     popupContent += '</div>';
